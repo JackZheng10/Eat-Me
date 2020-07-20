10002154eda8442e3862b0153884a6951a9d82f6
Jack Zheng <jackzheng10@yahoo.com>
2020-07-20 05:18:04 -0400

    Reconfigured auth helpers in prep for use

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -3,35 +3,53 @@ const jwt = require("jsonwebtoken"),
   jwt_secret = process.env.secret || require("../config/config.js").jwt.secret;
 
 // function to create tokens, access time is 24 hrs by default
-function signToken(user) {
+const signToken = (user) => {
   const userData = user.toObject();
   delete userData.password;
   return jwt.sign(userData, jwt_secret); //formats it into the JWT encoded xx.xx.xx, to be served to frontend if needed
-}
+};
 
 // function to verify tokens
-function verifyToken(req, res, next) {
+const verifyToken = async (req, res, next) => {
   const token = req.get("token") || req.body.token || req.query.token;
 
   // reject user if no token
-  if (!token) return res.json({ success: false, message: "No token provided" });
+  if (!token) {
+    return res.json({ success: false, message: "No token provided." });
+  }
 
   // try to verify token
-  jwt.verify(token, jwt_secret, (err, decodedData) => {
+  jwt.verify(token, jwt_secret, async (err, decodedData) => {
     // error check
-    if (err) return res.json({ success: false, message: "Error with token" });
+    if (err) {
+      return res.json({
+        success: false,
+        message: "Error with token. Please relog and try again.",
+      });
+    }
 
     // find user associated with token
-    User.findById(decodedData._id, (err, user) => {
-      // reject token if no user
-      if (!user)
-        return res.json({ success: false, message: "Error with token" });
+    try {
+      const user = await User.findById(decodedData._id);
 
-      req.user = user;
-      next();
-    });
+      if (!user) {
+        return res.json({
+          success: false,
+          message: "Error with token. Please relog and try again.",
+        });
+      } else {
+        res.locals.tokenUser = user;
+        next();
+      }
+    } catch (error) {
+      console.log("Error with finding user associated with token: " + error);
+      return res.json({
+        success: false,
+        message: "Error with token. Please relog and try again.",
+      });
+    }
   });
-}
+};
 
 module.exports = {
   signToken,
