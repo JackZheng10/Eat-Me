6218498ce4f04decf1e356b98095d5dfcd00961a
Jack Zheng <jackzheng10@yahoo.com>
2020-07-26 18:24:58 -0400

    Styling to editing account info

--- client/src/components/MainApp/components/Settings/components/Account/components/Phone.js
+++ client/src/components/MainApp/components/Settings/components/Account/components/Phone.js
@@ -1,9 +1,95 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, Dimensions } from "react-native";
+import { Input, Button, Text } from "react-native-elements";
+
+const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get("window").width;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F5F1ED",
+  },
+  scrollContainer: {
+    width: "100%",
+  },
+  inputContainer: {
+    width: windowWidth - 50,
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingTop: 25,
+    marginBottom: 20,
+    marginTop: 20,
+    borderRadius: 50,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+    elevation: 10,
+  },
+  containerStyle: {
+    width: windowWidth - 150,
+  },
+  inputContainerStyle: {
+    borderRadius: 40,
+    borderWidth: 1,
+    borderColor: "#00B2CA",
+    paddingHorizontal: 10,
+  },
+  inputStyle: {
+    marginLeft: 5,
+    marginRight: 5,
+    color: "black",
+  },
+  labelStyle: {
+    color: "black",
+  },
+  buttonContainerStyle: {
+    width: windowWidth - 250,
+    marginBottom: 20,
+  },
+  buttonContainerStyleReg: {
+    width: windowWidth - 250,
+  },
+  buttonStyle: {
+    backgroundColor: "#F79256",
+    borderRadius: 40,
+  },
+});
 
 class Phone extends Component {
   render() {
-    return <Text>phone</Text>;
+    return (
+      <View style={styles.mainContainer}>
+        <View style={styles.inputContainer}>
+          <Input
+            placeholder="Phone"
+            leftIcon={{
+              type: "material-community",
+              name: "phone",
+              color: "#00B2CA",
+            }}
+            onChange={null}
+            containerStyle={styles.containerStyle}
+            inputContainerStyle={styles.inputContainerStyle}
+            inputStyle={styles.inputStyle}
+            labelStyle={styles.labelStyle}
+            placeholderTextColor="#00B2CA"
+          />
+          <Button
+            title="Save"
+            raised
+            containerStyle={styles.buttonContainerStyle}
+            buttonStyle={styles.buttonStyle}
+          />
+        </View>
+      </View>
+    );
   }
 }
 
